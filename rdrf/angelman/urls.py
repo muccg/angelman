de73b2786d4f681a98db9716ca793d0e372aeaad
Maciej Radochonski <mradochonski@gmail.com>
2016-08-04 14:22:32 +0800

    Working state

--- rdrf/angelman/urls.py
+++ rdrf/angelman/urls.py
@@ -5,10 +5,10 @@ import patient_view
 
 urlpatterns = patterns('',
                        url(r"^(?P<registry_code>\w+)/parent/?$",
-                           parent_view.ParentView.as_view(), name='parent_page'))
+                           parent_view.ParentView.as_view(), name='parent_page'),
 
-#                       url(r"^(?P<registry_code>\w+)/parent/(?P<parent_id>\d+)/?$",
-#                           parent_view.ParentEditView.as_view(), name='parent_edit'),
+                       url(r"^(?P<registry_code>\w+)/parent/(?P<parent_id>\d+)/?$",
+                           parent_view.ParentEditView.as_view(), name='parent_edit'))
 
 #                       url(r"^(?P<registry_code>\w+)/patient/?$",
 #                           patient_view.PatientView.as_view(), name='patient_page'))
