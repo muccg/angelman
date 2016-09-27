c591c686c89038af78eb9e8a7b12329c78e9be98
Lee Render <id2359@yahoo.com>
2016-09-27 11:42:57 +0800

    Bumped version to 1.6.9

--- angelman/setup.py
+++ angelman/setup.py
@@ -39,7 +39,7 @@ for package in ['angelman']:
 
 
 setup(name='django-angelman',
-      version="1.6.8",
+      version="1.6.9",
       packages=packages,
       description='RDRF',
       long_description='Rare Disease Registry Framework',
