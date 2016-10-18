e1b32ec2b85b1c1902a2de4d18d2766066a8b379
Rodney Lorrimar <rlorrimar@ccg.murdoch.edu.au>
2016-10-18 16:42:48 +0100

    settings: avoid migrations from notifications/languages
    These settings are causing django to make migrations.
    
    Remove them for now. Models will need some fixes to allow customization
    of languages and e-mail notifications.
    
    See muccg/fkrp#384.

--- angelman/angelman/settings.py
+++ angelman/angelman/settings.py
@@ -15,18 +15,10 @@ TEMPLATES[0]['DIRS'] = [
     os.path.join(ANGELMAN_WEBAPP_ROOT, FALLBACK_REGISTRY_CODE, 'templates'),
 ] + TEMPLATES[0]['DIRS']
 
-LANGUAGES += (
-    ('no', 'Norwegian'),
-)
-
 ROOT_URLCONF = '%s.urls' % FALLBACK_REGISTRY_CODE
 
 LOGIN_FAILURE_LIMIT = 3
 SEND_ACTIVATION_EMAIL = False
-
-EMAIL_NOTIFICATIONS += (
-    ("account-locked", "Account Locked"),
-)
 
 RECAPTCHA_SITE_KEY = env.get("recaptcha_site_key", "")
 RECAPTCHA_SECRET_KEY = env.get("recaptcha_secret_key", "")
