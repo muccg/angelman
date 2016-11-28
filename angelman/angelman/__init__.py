6d1f0bebf95dd3cbe71b7ce1266199a119b4a77f
aahunter <ahunter@ccg.murdoch.edu.au>
2016-11-28 18:20:38 +0800

    Revert "Bumped version to 1.7.5"
    This reverts commit 3505894d3dc1d0d8b065a4692134d9773c052c52.

--- angelman/angelman/__init__.py
+++ angelman/angelman/__init__.py
@@ -1,2 +1,2 @@
-VERSION = "1.7.5"
+VERSION = "1.7.4"
 __version__ = VERSION
--- angelman/setup.py
+++ angelman/setup.py
@@ -39,7 +39,7 @@ for package in ['angelman']:
 
 
 setup(name='django-angelman',
-      version="1.7.5",
+      version="1.7.4",
       packages=packages,
       description='RDRF',
       long_description='Rare Disease Registry Framework',
