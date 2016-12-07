99f5bfdd93191e84e51ad6de4f8044ab31ede0ed
aahunter <ahunter@ccg.murdoch.edu.au>
2016-12-07 15:13:00 +0800

    version bump 1.7.7

--- angelman/setup.py
+++ angelman/setup.py
@@ -39,7 +39,7 @@ for package in ['angelman']:
 
 
 setup(name='django-angelman',
-      version="1.7.6",
+      version="1.7.7",
       packages=packages,
       description='RDRF',
       long_description='Rare Disease Registry Framework',
