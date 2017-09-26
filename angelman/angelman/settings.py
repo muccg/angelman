cfe50a8a75632e943f39ea37cf0727e0c9cd9987
lee <id2359@yahoo.com>
2017-09-26 10:36:08 +0800

    angelman #217 Added in Angelman password rules to settings

--- angelman/angelman/settings.py
+++ angelman/angelman/settings.py
@@ -26,3 +26,21 @@ RECAPTCHA_SECRET_KEY = env.get("recaptcha_secret_key", "")
 
 PROJECT_TITLE = "Global Angelman Syndrome Registry"
 PROJECT_TITLE_LINK = "login_router"
+
+
+
+
+# For now keep MTM password validation same as on front end
+# at least 6 chars
+# has a number
+AUTH_PASSWORD_VALIDATORS = [{
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 6,
+        }
+    },
+    {
+        'NAME': 'rdrf.auth.password_validation.HasNumberValidator',
+    },
+]
+
