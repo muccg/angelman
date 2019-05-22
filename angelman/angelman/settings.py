ff3335750e19786dd270ca73cb3c4cd4d1189dc3
Tamas Szabo <tszabo@ccg.murdoch.edu.au>
2019-05-22 18:04:11 +0300

    Removed unused settings and templatetag.

--- angelman/angelman/settings.py
+++ angelman/angelman/settings.py
@@ -4,25 +4,17 @@ import os
 from rdrf.settings import *
 import angelman
 
-# Insert fkrp before rdrf and the rest of apps. This ensures templates
-# and translations of fkrp get loaded first.
 
 FALLBACK_REGISTRY_CODE = "angelman"
 LOCALE_PATHS = env.getlist("locale_paths", ['/data/translations/locale'])
 
+# Adding the angelman app first, so that its templates overrides base templates
 INSTALLED_APPS = [
     FALLBACK_REGISTRY_CODE,
 ] + INSTALLED_APPS
 
-# Angelman WEBAPP_ROOT is different to RDRF base. This allows to find overriden templates
-ANGELMAN_WEBAPP_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATES[0]['DIRS'] = [
-    os.path.join(ANGELMAN_WEBAPP_ROOT, FALLBACK_REGISTRY_CODE, 'templates'),
-    ] + TEMPLATES[0]['DIRS']
-
 ROOT_URLCONF = '%s.urls' % FALLBACK_REGISTRY_CODE
 
-LOGIN_FAILURE_LIMIT = 3
 SEND_ACTIVATION_EMAIL = False
 
 RECAPTCHA_SITE_KEY = env.get("recaptcha_site_key", "")
@@ -36,7 +28,7 @@ PROJECT_TITLE_LINK = "login_router"
 REGISTRATION_CLASS = "angelman.patient_registration.AngelmanRegistration"
 
 
-# For now keep MTM password validation same as on front end
+# For now keep password validation same as on front end
 # at least 6 chars
 # has a number
 AUTH_PASSWORD_VALIDATORS = [{
