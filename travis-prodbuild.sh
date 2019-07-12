95a1614deb8cc9ee6c1e86a86e1d2b22682279b7
lee <id2359@yahoo.com>
2019-07-12 11:25:12 +0800

    fix build

--- travis-prodbuild.sh
+++ travis-prodbuild.sh
@@ -10,11 +10,7 @@ if [ x"$BRANCH_NAME" != x"master" -a x"$BRANCH_NAME" != x"next_release" -a x"$BR
     echo "Branch $BRANCH_NAME is not deployable. Skipping prod build and tests"
     exit 0
 fi
-
-#./develop.sh run-builder checkout
-./develop.sh run build node
-./develop.sh run-builder releasetarball
-sudo chown -R 1000 build
+./develop.sh run-builder
 ./develop.sh build prod
 ./develop.sh aloe prod
 ./develop.sh push prod
