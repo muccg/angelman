7ebd17f79715c3ae0e38e83365ad42bb47280d18
lee <id2359@yahoo.com>
2020-03-17 14:35:48 +0800

    Bump to 6.1.9 Use base 6.1.9

--- angelman/setup.py
+++ angelman/setup.py
@@ -39,7 +39,7 @@ for package in ['angelman']:
 
 
 setup(name='django-angelman',
-      version="6.1.8",
+      version="6.1.9",
       packages=packages,
       description='RDRF',
       long_description='Rare Disease Registry Framework',
